1957d4e8c4a43e9c4bc3c965e9cdb1fc4dc10eec
Jae Anne Bach Hardie <jhardie@findmypast.com>
2019-01-09 15:12:47 +0000

    Day 1 plus

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,38 @@
-pub fn std<I>(input: I) -> std::result::Result<i32, std::num::ParseIntError>
+use std::collections::HashSet;
+
+pub fn std<I>(input: I) -> Option<i32>
 where
   I: IntoIterator<Item = String>,
 {
+  Some(
+    input
+      .into_iter()
+      .map(|x| x.parse::<i32>().unwrap())
+      .fold(0, |acc, x| acc + x),
+  )
+}
+
+pub fn plus<I>(input: I) -> Option<i32>
+where
+  I: IntoIterator<Item = String>,
+  <I as std::iter::IntoIterator>::IntoIter: std::clone::Clone,
+{
+  let mut seen = HashSet::new();
+  let mut current = 0;
+  seen.insert(current);
   input
     .into_iter()
-    .map(|x| x.parse::<i32>())
-    .fold(Ok(0), |acc, x| Ok(acc? + x?))
+    .map(|x| x.parse::<i32>().unwrap())
+    .cycle()
+    .find_map(|x| {
+      current = current + x;
+      if seen.contains(&current) {
+        Some(current)
+      } else {
+        seen.insert(current);
+        None
+      }
+    })
 }
 
 #[cfg(test)]
@@ -15,33 +42,91 @@ mod tests {
   #[test]
   fn std_example_1() {
     let input = vec![
-      String::from("1"),
+      String::from("+1"),
       String::from("-2"),
-      String::from("3"),
-      String::from("1"),
+      String::from("+3"),
+      String::from("+1"),
     ];
-    let expected = Ok(3);
+    let expected = Some(3);
     assert_eq!(std(input), expected);
   }
 
   #[test]
   fn std_example_2() {
-    let input = vec![String::from("1"), String::from("1"), String::from("1")];
-    let expected = Ok(3);
+    let input = vec![String::from("+1"), String::from("+1"), String::from("+1")];
+    let expected = Some(3);
     assert_eq!(std(input), expected);
   }
 
   #[test]
   fn std_example_3() {
-    let input = vec![String::from("1"), String::from("1"), String::from("-2")];
-    let expected = Ok(0);
+    let input = vec![String::from("+1"), String::from("+1"), String::from("-2")];
+    let expected = Some(0);
     assert_eq!(std(input), expected);
   }
 
   #[test]
   fn std_example_4() {
     let input = vec![String::from("-1"), String::from("-2"), String::from("-3")];
-    let expected = Ok(-6);
+    let expected = Some(-6);
     assert_eq!(std(input), expected);
+  }
+
+  #[test]
+  fn plus_example_1() {
+    let input = vec![
+      String::from("+1"),
+      String::from("-2"),
+      String::from("+3"),
+      String::from("+1"),
+    ];
+    let expected = Some(2);
+    assert_eq!(plus(input), expected);
+  }
+
+  #[test]
+  fn plus_example_2() {
+    let input = vec![String::from("+1"), String::from("-1")];
+    let expected = Some(0);
+    assert_eq!(plus(input), expected);
+  }
+
+  #[test]
+  fn plus_example_3() {
+    let input = vec![
+      String::from("+3"),
+      String::from("+3"),
+      String::from("+4"),
+      String::from("-2"),
+      String::from("-4"),
+    ];
+    let expected = Some(10);
+    assert_eq!(plus(input), expected);
+  }
+
+  #[test]
+  fn plus_example_4() {
+    let input = vec![
+      String::from("-6"),
+      String::from("+3"),
+      String::from("+8"),
+      String::from("+5"),
+      String::from("-6"),
+    ];
+    let expected = Some(5);
+    assert_eq!(plus(input), expected);
+  }
+
+  #[test]
+  fn plus_example_5() {
+    let input = vec![
+      String::from("+7"),
+      String::from("+7"),
+      String::from("-2"),
+      String::from("-7"),
+      String::from("-4"),
+    ];
+    let expected = Some(14);
+    assert_eq!(plus(input), expected);
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file = File::open(String::from("input/") + &args[1]).unwrap();
     let buf_reader = BufReader::new(file);
-    let input = buf_reader.lines().map(|x| x.unwrap());
+    let input: Vec<String> = buf_reader.lines().map(|x| x.unwrap()).collect();
     let args_ref: Vec<&str> = args.iter().map(|x| x.as_str()).collect();
     let result = match &args_ref[..] {
         [_, "day1", "std"] => day1::std(input),
+        [_, "day1", "plus"] => day1::plus(input),
         _ => panic!("Function not found"),
     };
     println!("The result is {}", result.unwrap());
